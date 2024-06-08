3d67b2969f1fcdfafb23ecd295a7cabce9c64941
Toni Graphics <buch.toni@outlook.com>
2024-06-08 20:20:44 +0200

    organized code + added cpp support

---
+++ kernel/src/_entry.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <limine.h>
+
+__attribute__((used, section(".requests")))
+static volatile LIMINE_BASE_REVISION(2);
+
+// Halt and catch fire function.
+static void hcf(void) {
+    asm ("cli");
+    for (;;) {
+        asm ("hlt");
+    }
+}
+
+extern void krnl();
+extern void init();
+
+void _start(void) {
+    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    init();
+    krnl();
+
+    hcf();
+}
